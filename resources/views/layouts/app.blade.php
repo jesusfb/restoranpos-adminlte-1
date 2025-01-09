0b2bdd2fadc2397247f0a58e21f8722f7e760ecf
jesusfb <jfigueroa.beltran@gmail.com>
2025-01-09 15:10:56 -0800

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,17 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
+    <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>25</h3>
+
+                        <p>Platos</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="platos.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
     <a href="/dashboard" class="brand-link elevation-4">
       <img src="/images/restorantpos.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">RestoranPOS</span>
