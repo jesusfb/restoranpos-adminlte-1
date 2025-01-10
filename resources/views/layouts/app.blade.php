709184f7a6168dd311871e4ac2adc19d2a9157ea
jesusfb <jfigueroa.beltran@gmail.com>
2025-01-10 14:02:14 -0800

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i style="font-size:32px" class="fas fa-bars"></i></a>
                 </li>
             </ul>
 
