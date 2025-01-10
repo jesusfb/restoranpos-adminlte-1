ced22399e93b00835bc29c4b35ba5076df4a0ce4
jesusfb <jfigueroa.beltran@gmail.com>
2025-01-10 14:11:08 -0800

    first commit

--- resources/views/platillos/edit.blade.php
+++ resources/views/platillos/edit.blade.php
@@ -15,7 +15,7 @@
                     </div>
                     <div class="card-body bg-white">
                         <form method="POST" action="{{ route('platillos.update', $platillo->id) }}"  role="form" enctype="multipart/form-data">
-                            {{ method_field('PATCH') }}
+                            {{ method_field('POST') }}
                             @csrf
 
                             @include('platillos.form')
