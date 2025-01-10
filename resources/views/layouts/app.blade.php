449056fcfb0cefb2a4a8443e03e090fa3309f625
jesusfb <jfigueroa.beltran@gmail.com>
2025-01-09 21:05:47 -0800

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <link rel="icon" type="image/x-icon" href="https://jesusfb.github.io/velozcdn/favicon.ico" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Restoran | POS</title>
 
